7c2a1bf85f1d317d1e861f143e2a067c2a7648ef
odjhey <odjhey@gmail.com>
2023-08-17 09:57:57 +0800

    chore: add formatter

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-
-
 import unittest
 
 
 def add(x, y):
-	return x+y
-
-class TestSome (unittest.TestCase):
-	def test_t1(self):
-		expect = 10
-		result = add(2,8)
-		self.assertEqual(expect, result, "Should be 10")
+    return x + y
 
 
-if __name__ == '__main__':
-	unittest.main()
+class TestSome(unittest.TestCase):
+    def test_t1(self):
+        expect = 10
+        result = add(2, 8)
+        self.assertEqual(expect, result, "Should be 10")
+
+
+if __name__ == "__main__":
+    unittest.main()
